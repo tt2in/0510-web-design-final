d481ffa0dec0e791b28c16babddeb08d07c52298
tt2in <kcoolk22@naver.com>
2023-05-10 12:50:20 +0900

    state

--- script/common.js
+++ script/common.js
@@ -28,3 +28,8 @@ $(function(){
         $('#modal').removeClass('active')
     })
 })
+
+// index 파일에서 js 작성할때는 <scrip></script>에
+// 바로 원하는 부분부터 작성해주면 된다. 하지만,
+// js 파일을 따로 작성할 때 무조건 $(function)(){~}
+// 안에 작성한 코드들을 넣어주면 된다
